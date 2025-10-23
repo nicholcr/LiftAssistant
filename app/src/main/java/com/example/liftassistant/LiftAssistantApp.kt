54956c7d0c48bb44715e77661aa7019fa02db269
nicholcr <nicholcr@umich.edu>
2025-10-23 16:12:32 -0500

    add top app bar and body to home screen

--- app/src/main/java/com/example/liftassistant/LiftAssistantApp.kt
+++ app/src/main/java/com/example/liftassistant/LiftAssistantApp.kt
@@ -1,2 +1,55 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.liftassistant
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.liftassistant.ui.navigation.LiftAssistantNavHost
+import com.example.liftassistant.R.string
+
+/**
+ * Top level composable that represents screens for the application.
+ */
+@Composable
+fun LiftAssistantApp(navController: NavHostController = rememberNavController()) {
+    LiftAssistantNavHost(navController = navController)
+}
+
+/**
+ * App bar to display title and conditionally display the back navigation.
+ */
+@Composable
+fun LiftAssistantTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
--- app/src/main/java/com/example/liftassistant/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/liftassistant/ui/home/HomeScreen.kt
@@ -1,11 +1,25 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.liftassistant.ui.home
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.liftassistant.LiftAssistantTopAppBar
 import com.example.liftassistant.ui.AppViewModelProvider
 import com.example.liftassistant.ui.navigation.NavigationDestination
 import com.example.liftassistant.R
+import com.example.liftassistant.data.Workout
 
 object HomeDestination : NavigationDestination {
     override val route = "home"
@@ -16,6 +30,33 @@ object HomeDestination : NavigationDestination {
 fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    val homeUiState by viewModel.homeUiState.collectAsState()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            LiftAssistantTopAppBar(
+                title = stringResource(HomeDestination.titleRes),
+                canNavigateBack = false,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
+        HomeBody(
+            workoutList = homeUiState.workoutList,
+            modifier = modifier.fillMaxSize(),
+            contentPadding = innerPadding,
+        )
+    }
+}
+
+@Composable
+private fun HomeBody(
+    workoutList: List<Workout>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
 ) {
 
 }
